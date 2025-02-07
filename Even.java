00f0318f7ed426a760f433f53bc8d55d05a4446f
ashwini9009 <rukareashwini@gmail.com>
2025-02-07 15:43:33 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,19 @@
+package com.tka.nov28;
+
+public class Even {
+	
+		  void m1()
+		{
+		for(int i=103;i<=157;i++)
+		{
+			if( i%2==0)
+			{
+				System.out.println(i);
+			}
+		}
+		}
+
+	}
+
+
+
---
+++ TestEven.java
@@ -0,0 +1,11 @@
+package com.tka.nov28;
+
+public class TestEven {
+	public static void main(String args[])
+	{
+		Even e=new Even();
+		System.out.println("Even numbers from 103 to 157 are: \n");
+		e.m1();
+	}
+
+}
